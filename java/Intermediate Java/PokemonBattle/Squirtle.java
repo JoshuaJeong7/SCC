e53e3ad44c9a58023de65cf2e8a96a8a20037c38
JoshuaJeong7 <jeong.joshua275@gmail.com>
2025-05-16 18:18:08 -0700

    Update Squirtle.java

--- java/Intermediate Java/PokemonBattle/Squirtle.java
+++ java/Intermediate Java/PokemonBattle/Squirtle.java
@@ -24,19 +24,14 @@ public class Squirtle extends Pokemon {
     }
     
     public void fight(Pokemon other) {
-        //NOTE: The red comments are ALL that the students will see upon first
-        //        receiving the program.
-        
-        //Scanner moveInput = new Scanner(System.in);
-        
-        //System.out.println("Which move will you attack with, Squirtle? (enter number)");
-        //System.out.println("1) [move name]");
+
         
         Scanner moveInput = new Scanner(System.in);
         
         System.out.println("Which move will you attack with, Squirtle? (enter number)");
         System.out.println("1) Tackle");
         System.out.println("2) Tail Whip");
+        System.out.println("Add more moves in the code!");
         
         int chosenMove = moveInput.nextInt();
         
