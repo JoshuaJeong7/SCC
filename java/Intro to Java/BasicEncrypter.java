72239333cb6d37e3573c92fca827989aba3d4505
JoshuaJeong7 <jeong.joshua275@gmail.com>
2025-09-03 23:50:15 -0700

    Update BasicEncrypter.java
    Major rework of what the program is assessing you to do

--- java/Intro to Java/BasicEncrypter.java
+++ java/Intro to Java/BasicEncrypter.java
@@ -1,72 +1,38 @@
 //import java.util.Scanner;
 /**
- * UNIT: Scanner (and Strings)
+ * UNIT: Encryption
  * 
  * ASSESSMENT ON: 
- * -Ability to use Scanner class
+ * -Encryption (the knowledge of ASCII)
+ * -Ability to utilize loops 
  * -Ability to use the substring function with Strings
- * 
- * NOT ASSESSED AT ALL:
- * -The encryption itself (i.e. the knowledge of ASCII and char values)
- * -The usage of methods
- * -If statements
  */
 public class BasicEncrypter {
     public static void main(String[] args) {
-        //Everything commented out is the intended solution that the students must code.
-        
-        //Scanner in = new Scanner(System.in);
-        
-        System.out.println("This program takes in a string and encrypts it," +
-            " or shifts all letters two positions down the alphabet. \n" + 
-            "For example, \'A\' will become \'C\', \'D\' will become \'F\', " +
-            " and so on. \n At the end of the alphabet, \'X\' becomes \'A\' and " +
-            " \'Z\' becomes \'B\'. \n Non-letters (numbers, symbols, etc.)" +
-            " will not be shifted at all. \n \n");
         System.out.println("Enter your 5-letter word to encrypt:");
-        //String input = in.next();
+        String input = in.next();
         
-        String letter1 = //input.substring(0, 1); 
-        String letter2 = //input.substring(1, 2); 
-        String letter3 = //input.substring(2, 3); 
-        String letter4 = //input.substring(3, 4); 
-        String letter5 = //input.substring(4); 
-        
-        System.out.println("The encrypted word:");
-        encryptLetterAndPrint(letter1);
-        encryptLetterAndPrint(letter2);
-        encryptLetterAndPrint(letter3);
-        encryptLetterAndPrint(letter4);
-        encryptLetterAndPrint(letter5);
+        encryptAndPrint(input);
         
         System.out.println();
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     /**
-     * NOTE TO STUDENTS: You do not have to worry about this code. All
-     * this function does is do the encryption of ONE letter. You may try to
-     * understand it if you want, but it is not necessary to edit 
-     * this function in any other way.
+     * NOTE TO STUDENTS: This is the method where you need to finish
+     * the encryption code!
+     * If you do not know what encryption is, it is basically shifting
+     * the letters of a character 2 letters down the alphabet.
+     * Ex: A -> C; E -> G; Z -> B; X -> A (In the last two cases, we wrapped back to A)
+     * Non-letters (numbers, symbols, etc.) will not be shifted at all.
      */
-    public static void encryptLetterAndPrint(String word) {
-        char letter = word.charAt(0);
-        
-        if (letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z') {
-            letter += 2;
-            if ((letter >'Z' && letter < 'a') || (letter > 'Z' && letter < 'a' || letter > 'z'))
-                letter -= 26;
+    public static void encryptAndPrint(String word) {
+        System.out.println("Before encryption: " + word);
+        String newWord = "";
+        for (int i = 0; i < word.length; i++) {
+            char currLetter = newWord.charAt(i);
+
+            //FINISH THE ENCRYPTION HERE
         }
-        System.out.print(letter);
+        System.out.print("After encryption: " + newWord);
     }
 }
