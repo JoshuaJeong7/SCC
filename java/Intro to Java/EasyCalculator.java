e323ebf954776d8e4bd03ae9797c0b66cbcb0c46
JoshuaJeong7 <jeong.joshua275@gmail.com>
2025-10-16 22:23:14 -0700

    Create EasyCalculator.java
    New easier level program for students. Students do many arithmetic operations like making calculator functions.

---
+++ java/Intro to Java/EasyCalculator.java
@@ -0,0 +1,30 @@
+public class EasyCalculator {
+    public static void main(String[] args) {
+        int num1 = 4; //Change these numbers if you want to use different ones!
+        int num2 = 3; //Change these numbers if you want to use different ones!
+
+        //In one line, add num1 and num2 together.
+        int sum = //FINISH THE TASK
+        System.out.println("Sum of num1 and num2: " + sum);
+
+        //In one line, divide num1 by num2.
+        int quotient = //FINISH THE TASK
+        System.out.println("Quotient of num1 and num2: " + quotient);
+
+        //In one line, find the remainder of the division between num1 and num2.
+        int modulus = //FINISH THE TASK
+        System.out.println("Modulus of num1 and num2: " + modulus);
+
+        //In one line, return the absolute value of num1.
+        int absValue = //FINISH THE TASK
+        System.out.println("Absolute value of num1: " + absValue);
+        
+        //[CHALLENGE] In one line, raise num1 to the 'num2'th power.
+        int exponent = //FINISH THE TASK
+        System.out.println("Power of num1 and num2: " + exponent);
+
+        //[CHALLENGE] In one line, return the cosine of num1 in radians.
+        int cosine = //FINISH THE TASK
+        System.out.println("Cosine with angle num1 (radians): " + cosine);
+    }
+}
