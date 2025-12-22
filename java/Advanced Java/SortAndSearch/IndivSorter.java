cc2c8c69449c2846fc4051cce8a7fb066387c031
JoshuaJeong7 <jeong.joshua275@gmail.com>
2025-12-21 22:40:47 -0800

    Add files via upload

---
+++ java/Advanced Java/SortAndSearch/IndivSorter.java
@@ -0,0 +1,6 @@
+public abstract class IndivSorter {
+    public IndivSorter() {
+
+    }
+    public abstract void thisSort(int[] arr);
+}
---
+++ java/Advanced Java/SortAndSearch/InsertionSortIterative.java
@@ -0,0 +1,7 @@
+public class InsertionSortIterative extends IndivSorter {
+    public void thisSort(int[] arr) {
+        System.out.println("No code implemented!\nWrite a body method for this sort in IterationSortIterative.java!\nDelete this print statement once you are done.");
+        //Write your InsertionSort code here:
+
+    }
+}
