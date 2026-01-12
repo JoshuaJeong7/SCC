44f01f10c942c12fe5f36f461c01e70c15c4049a
JoshuaJeong7 <jeong.joshua275@gmail.com>
2026-01-12 12:18:04 -0800

    Completed basic framework for TwentyElementList.java
    Added linked list and add method

--- java/Advanced Java/Reference/LinkedLists/TwentyElementList.java
+++ java/Advanced Java/Reference/LinkedLists/TwentyElementList.java
@@ -1,8 +1,37 @@
 class ElementList {
-  String message;
-}
-public class TwentyElementList {
-  public static void main(String[] args) {
-
+  public String message;
+  public ElementList next;
+  public ElementList(String msg) {
+    message = msg;
+    next = null;
   }
 }
+
+public class TwentyElementList {
+  public static void addElement(ElementList root, String val) {
+     // if (root == null) root = new ElementList(val);
+     // else {
+          ElementList temp = root;
+          while (temp.next != null) {
+              temp = temp.next;
+          }
+          temp.next = new ElementList(val);
+      //}
+  }
+  public static void main(String[] args) {
+    String[] messageVals = {"corn", "bag", "apple", "pizza", "mace", "door", "window", "college", "bus", "beautiful", "sad", "angry", "cheese", "games", "sudoku", "mine", "gold", "fracture", "gym", "classroom"};
+    ElementList root = new ElementList(messageVals[0]);
+    for (int i = 1; i < messageVals.length; i++) {
+      addElement(root, messageVals[i]);
+    }
+
+    //for testing
+    ElementList temp = root;
+    while (temp != null) {
+      System.out.print(temp.message + " ");
+      temp = temp.next;
+    }
+  }
+}
+
+
