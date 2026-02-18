b29e18e56961ccec1daefdac5034a3ab63fd3588
JoshuaJeong7 <jeong.joshua275@gmail.com>
2026-02-18 00:46:04 -0800

    Add compareTo method for NBAPlayer
    Implement compareTo method in NBAPlayer class for students to compare players based on free throws, games won, and total score.

--- java/Advanced Java/Comparable/NBAShowdown.java
+++ java/Advanced Java/Comparable/NBAShowdown.java
@@ -1,3 +1,25 @@
-public class NBAShowdown {
+//NOTE: If you are copying this into Programiz, move this class definition to the bottom of the file!
+//Then your class errors should be fixed!
+class NBAPlayer implements Comparable<NBAPlayer> {
+    int freeThrows;
+    int gamesWon;
+    int totalScore;
 
+    // Within the NBAPlayer class, write a compareTo() method that does the following:
+    // - Find the difference both players’ free throw count. Whoever has more free throws earns a point.
+    // - Find the difference both players’ games won. Whoever has more games won earns a point.
+    // - Find the difference both players’ total scores. Whoever has more total scores earns a point.
+    // - Find the difference in both players’ points across these three categories. Whoever has more points is the better NBA player!
+    public int compareTo(NBAPlayer other) {
+        //Finish your code here!
+
+        
+        return -1;
+    }
+}
+
+public class NBAShowdown {
+    public static void main(String[] args) {
+
+    }
 }
