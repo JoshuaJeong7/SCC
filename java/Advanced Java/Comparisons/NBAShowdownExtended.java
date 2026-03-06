e12b2431ab4523281a7b33be906e1746dd9c13d7
JoshuaJeong7 <jeong.joshua275@gmail.com>
2026-03-06 11:19:53 -0800

    Copy NBAPlayer class and add FreeThrowVersus Comparator

--- java/Advanced Java/Comparisons/NBAShowdownExtended.java
+++ java/Advanced Java/Comparisons/NBAShowdownExtended.java
@@ -1,3 +1,20 @@
+//NOTE: If you are copying this into Programiz, move this class definition to the bottom of the file!
+//Then your class errors should be fixed!
+class NBAPlayer {
+    int freeThrows;
+    int gamesWon;
+    int totalScore;
+    public NBAPlayer(int f, int g, int s) {
+        freeThrows = f;
+        gamesWon = g;
+        totalScore = s;
+    }
+}
+
 public class NBAShowdownExtended {
+
+}
+
+class freeThrowVersus extends Comparator<NBAPlayer> {
 
 }
